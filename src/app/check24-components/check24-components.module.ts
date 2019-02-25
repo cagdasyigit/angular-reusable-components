8f12e4488a2e676fb2c8df6cccabeed026d0a642
cagdas.yigit <cagdas.yigit@pia-team.com>
2019-02-25 12:13:06 +0300

    developed a custom select instead of using native html select

--- src/app/check24-components/check24-components.module.ts
+++ src/app/check24-components/check24-components.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { Check24DropdownComponent } from './check24-dropdown/check24-dropdown.component';
+import { Check24DropdownComponent, Check24DropdownPipe } from './check24-dropdown/check24-dropdown.component';
 import { CommonModule } from '@angular/common';
 
 // May be we can add more components to the app, who knows?
 @NgModule({
     declarations: [
-        Check24DropdownComponent
+        Check24DropdownComponent,
+        Check24DropdownPipe
     ],
     imports: [
         CommonModule,
--- src/app/check24-components/check24-dropdown/check24-dropdown.component.ts
+++ src/app/check24-components/check24-dropdown/check24-dropdown.component.ts
@@ -1,37 +1,126 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, Pipe, PipeTransform } from '@angular/core';
 import { Check24Combobox } from '../check24-entities/check24-combobox';
 
 @Component({
     selector: 'check24-dropdown',
     templateUrl: './check24-dropdown.component.html',
-    styleUrls: ['./check24-dropdown.component.css']
+    styleUrls: ['./check24-dropdown.component.scss']
 })
 export class Check24DropdownComponent implements OnInit {
 
-    @Input('name') name: string;
+    @Input() name: string;
 
-    @Input('label') label: string;
+    @Input() label: string;
 
-    @Input('options') options: Check24Combobox[];
+    @Input() options: Check24Combobox[];
 
-    @Input('selectedOption') selectedOption: string;
+    @Input() selectedOption: string;
 
-    @Output('onSelected') onSelected: EventEmitter<string>;
+    @Output() selected: EventEmitter<string>;
+
+    public focused = false;
+
+    public selection = '';
 
     constructor() {
-        this.onSelected = new EventEmitter();
+        this.selected = new EventEmitter();
     }
 
     ngOnInit() {
         if (this.selectedOption == null) {
             this.selectedOption = '';
+        } else {
+            this.selection = this.selectedOption;
         }
     }
 
-    onChange(event: Event) {
-        const value = event.target['value'];
-
-        this.onSelected.emit(value);
+    onChange(code) {
+        this.selected.emit(code);
     }
 
+    onClickSelect() {
+        this.focused = !this.focused;
+    }
+
+    onClickOption(code: string) {
+        this.selectedOption = code;
+        this.selection = code;
+        this.onChange(code);
+    }
+
+    onKeydownSelect(event) {
+        switch (event.code.toLowerCase()) {
+            case 'enter':
+            case 'space':
+                this.focused = !this.focused;
+                this.selectedOption = this.selection;
+                this.onChange(this.selectedOption);
+                break;
+
+            case 'arrowdown':
+                this.moveSelection('next');
+                break;
+
+            case 'arrowup':
+                this.moveSelection('back');
+                break;
+
+            case 'escape':
+                this.focused = false;
+                break;
+        }
+    }
+
+    moveSelection(step: 'back' | 'next') {
+        // Options must be an array with items
+        if (this.options != null && this.options.length > 0) {
+            this.focused = true;
+
+            // If nothing selected yet
+            if (this.selection === '') {
+                switch (step) {
+                    case 'next':
+                        this.selection = this.options[0].code;
+                        break;
+
+                    case 'back':
+                        this.selection = this.options[this.options.length - 1].code;
+                        break;
+                }
+            } else {
+                // Or moving to another
+                const currentIndex = this.options.findIndex(option => option.code === this.selection);
+                const nextIndex = currentIndex + 1;
+                const backIndex = currentIndex - 1;
+
+                switch (step) {
+                    case 'next':
+                        if (this.options.length > nextIndex) {
+                            this.selection = this.options[nextIndex].code;
+                        } else {
+                            this.selection = this.options[0].code;
+                        }
+
+                        break;
+
+                    case 'back':
+                        if (backIndex > -1) {
+                            this.selection = this.options[backIndex].code;
+                        } else {
+                            this.selection = this.options[this.options.length - 1].code;
+                        }
+
+                        break;
+                }
+            }
+        }
+    }
+}
+
+@Pipe({ name: 'selectedValue' })
+export class Check24DropdownPipe implements PipeTransform {
+    transform(options: Check24Combobox[], code: string): string {
+        const selectedOption = options.find(option => option.code === code);
+        return selectedOption != null ? selectedOption.displayValue : 'Please select an option';
+    }
 }
